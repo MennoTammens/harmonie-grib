f79576ac8a8a99b61fa095251f014a05dfd9c08c
Jan Pieter Waagmeester <jieter@jieter.nl>
2016-05-17 10:07:03 +0200

    Add download script and Makefile

---
+++ Makefile
@@ -0,0 +1,22 @@
+TMPDIR := work
+
+convert:
+	mkdir -p $(TMPDIR)/
+	tar -xf harm36_v1_ned_surface_*.tgz -C $(TMPDIR)
+	./convert.py
+
+download:
+	./download.py
+
+ggrib:
+	wget http://ftp.cpc.ncep.noaa.gov/wd51we/gribw/ggrib/ggrib.tar.gz -O $(TMPDIR)/ggrib.tar.gz
+	tar -xf $(TMPDIR)/ggrib.tar.gz -C $(TMPDIR)
+
+	cp $(TMPDIR)/Ggrib/ggrib .
+
+clean:
+	rm -rf $(TMPDIR)/Ggrib
+	rm -rf $(TMPDIR)/Gribw
+	rm -rf $(TMPDIR)/ggrib.tar.gz
+	rm -rf $(TMPDIR)/harm36_v1_ned_surface_*
+	rm -rf temp.grb
---
+++ download.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+
+from datetime import datetime
+from ftplib import FTP
+
+FTP_HOST = 'data.knmi.nl'
+BASE_PATH = '/download/harmonie_p1/0.2/noversion/0000/00/00/'
+DATE_FMT = '%b %d %H:%M'
+
+ftp = FTP(FTP_HOST, 'anonymous')
+ftp.cwd(BASE_PATH)
+
+
+def file_list():
+    data = []
+    ftp.dir(data.append)
+
+    for item in data:
+        item = item.split()
+        mtime = datetime.strptime(' '.join(item[5:8]), DATE_FMT)
+
+        yield {
+            'name': item[-1],
+            'size': '{}MB'.format(int(int(item[4]) / 1024 / 1024)),
+            'mtime': mtime.replace(year=2016)  # TODO: fix year assumption
+        }
+
+
+def get_file(name):
+    ftp.retrbinary('RETR {}'.format(name), open(name, 'wb').write)
+
+
+recent = max(file_list(), key=lambda x: x['mtime'])
+
+
+print('Files available on server: ')
+for item in file_list():
+    print(item)
+print()
+
+print('Downloading most recent file:', recent['name'], 'size:', recent['size'])
+
+get_file(recent['name'])
+print('DONE')
